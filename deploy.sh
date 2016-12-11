7068ec3b5cc1e4c8f6e95ebc388ee1d7c3443637
kapadiamush <kapadiamush@gmail.com>
2016-12-10 18:45:32 -0800

    added deploy script and updated production start command in package.json

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+set -e
+
+cd /guilty-sparks/guilty-sparks/
+
+NEW_PROD=guilty-spark-`date +%Y-%m-%d_%H:%M:%S`
+
+git clone git@github.com:kapadiamush/guilty-spark.git $NEW_PROD
+(cd $NEW_PROD && npm install)
+
+ln -snf `pwd`/$NEW_PROD current
+
+cd current
+
+npm start > /var/log/guilty-spark/guilty-spark.log
