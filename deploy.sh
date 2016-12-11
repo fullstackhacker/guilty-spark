58ec51420957e268b4217c18172fb433e2a99579
kapadiamush <kapadiamush@gmail.com>
2016-12-10 19:09:02 -0800

    be sure to kill the old process

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,10 @@ ln -snf `pwd`/$NEW_PROD current
 
 cd current
 
+echo "Killing old one"
+pkill -f 'node guilty.js'
+echo "Killed"
+
+echo "Launching new one..."
 npm start > /var/log/guilty-spark/guilty-spark.log&
 echo "DONE!"
